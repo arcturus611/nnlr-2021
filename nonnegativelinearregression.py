ce72dcf393fc80b469f933f012fb3574c85d4d6d
arcturus611 <swati.6.itaws@gmail.com>
2021-08-20 17:16:56 -0500

    August 20 meeting updates

--- nonnegativelinearregression.py
+++ nonnegativelinearregression.py
@@ -2,6 +2,23 @@
 """
 nonnegative linear regression 
 """
+
+# Notes
+# 1. Note we set min A_ij = 1. This can be done either by dividing A by min Aij or by just adding 1. 
+# The first approach is kind of cheating because the scaling automatically makes the error small
+# If min Aij is not 1, then init error can be LARGE 
+
+#2. If all xj coordinates are updated in parallel, then comparable to scipy (update_v_parllel and update_X_paralle)
+
+#3. If coordinatewise update, then scaling down by n is better than not scaling. 
+
+#4. Our theoreitcal alg in experiment has occasional large jumps in error
+
+#5. The init large error issue is fixed by running a full update step first
+
+#6. x is very very very sparse
+
+#7. TODO m << n case 
    
 import numpy as np 
 from numpy import array  
@@ -17,16 +34,24 @@ def init_all(eps, n):
     xbar = np.zeros(n) 
     xkm = np.zeros(n)  
     x = np.zeros(n) 
-    ktotal = np.int(np.ceil(1/np.sqrt(eps))) 
+    ktotal = np.int(np.ceil(1/np.sqrt(eps))) #note that this is just an approx ktotal
     ak = 1 
     Ak = 1
-    A_sum_ai_xibar = 0 
-    v = np.zeros(n)
+    A_sum_ai_xibar = 0 #A\sum_i ai bar(x_{i-1})
+    v = np.zeros(n) #vector with same length as x, used to obtain x 
     return xbar, xkm, x, ktotal, ak, Ak, A_sum_ai_xibar, v
 
-# Add a scaling n in this code
+# (Aug 18) Add a scaling n in this code? 
 def update_v(jk, pjk, A, y, v, ak): 
-    v[jk]+= (ak/n/pjk)*((A.T).dot(y) - 1/n)[jk]
+    v[jk]+= (ak/pjk)*((A.T).dot(y) - 1)[jk] #scaling the step (making it larger)  to "compensate" for seq vs par
+    return v
+
+def update_v_smallstep(jk, pjk, A, y, v, ak): 
+    v[jk]+= (ak)*((A.T).dot(y) - 1)[jk] #scaling the step (making it larger)  to "compensate" for seq vs par
+    return v
+
+def update_v_parallel(A, y, v, ak): 
+    v+= ak*((A.T).dot(y) - 1)
     return v
 
 def compute_scaling(A):
@@ -37,6 +62,11 @@ def update_x(v, jk, xkm, scaling):
     x = xkm 
     x[jk]= np.max((scaling*v[jk], 0))
     return x      
+
+def update_x_parallel(v, svec): 
+    # x= np.max((svec*v, 0))
+    x = np.where(svec*v>0, svec*v, 0)
+    return x 
 
 def update_xbar(x, xkm, ak, akp):
     xbar = x + (ak/akp)*(x - xkm)
@@ -55,10 +85,11 @@ def update_y(A_sum_ai_xibar, A, ak, Ak, xbar):
 
 #%%
 if __name__ == '__main__': 
-    eps = 0.000001 
-    n = 100 # input dimension 
-    m = 500   
-    A = np.random.rand(m, n)
+    eps = 0.001 
+    n = 300 # input dimension 
+    m = 2000   
+    A = np.random.rand(m, n) # we want $min A_ij = 1$ 
+    A = A + 1
     xsum = 0 
 
 #%%
@@ -75,22 +106,38 @@ if __name__ == '__main__':
     Akarray[0]=Ak
 #%%         
     for k in range(ktotal): 
-        # sample jk from multinomial distribution
-        randomseed=np.random.multinomial(1, [1/n]*n)
-        jk = np.min(np.where(randomseed==1))
-        pjk = 1/n
-#%%         
-
-        # update y 
+                # update y 
         (y, A_sum_ai_xibar) = update_y(A_sum_ai_xibar, A, ak, Ak, xbar)
         y_norm[k]  = norm(y, 2)
-        # update v 
-        v = update_v(jk, pjk, A, y, v, ak)
-        v_norm[k]  = norm(v, 2)
-        # update x
-        x = update_x(v, jk, xkm, scaling_vector[jk])
-        x_norm[k]  = norm(x, 2)
+        
+        if (k==1):
+            v = update_v_parallel(A, y, v, ak)
+            x = update_x_parallel(v, scaling_vector)
+        else: 
+        # sample jk from multinomial distribution
+            randomseed=np.random.multinomial(1, [1/n]*n)
+            jk = np.min(np.where(randomseed==1))
+            pjk = 1/n
+#%%         #%%        
+        # # update v 
+            v = update_v(jk, pjk, A, y, v, ak)
+            v_norm[k]  = norm(v, 2)       
+                    
+            x = update_x(v, jk, xkm, scaling_vector[jk])
+
 #%%         
+        #update x parallelly 
+        # x = update_x_parallel(v, scaling_vector)
+            x_norm[k]  = norm(x, 2)
+#%%         
+#%% 
+        #update v parallelly 
+        # v = update_v_parallel(A, y, v, ak)
+        
+
+#%%   
+        # update x
+
         
         #update a 
         (Ak, akp) = update_ak_Ak(Ak, ak) 
@@ -115,7 +162,7 @@ if __name__ == '__main__':
     xsol = (1/(Ak-ak))*xsum 
 
     print("our result is "+ str(our_result[ktotal-1]))
-    plt.plot(range(ktotal), our_result, 'b', range(ktotal), x_norm, 'r') 
+    plt.plot(range(ktotal), our_result, 'b') 
     S=scipy.optimize.nnls(A/math.sqrt(2), A@inv(A.T@A)@np.ones(n)/math.sqrt(2), maxiter=ktotal)
     alg2_result = S[1]**2-norm(A@inv(A.T@A)@np.ones(n)/math.sqrt(2),2)**2
     print("their result is "+ str(alg2_result))
